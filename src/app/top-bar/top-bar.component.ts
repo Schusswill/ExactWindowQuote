47590e3f2057d6b78965a07d246b8df5f08bab36
Schusswill <will.schuss@gmail.com>
2019-10-11 21:24:38 -0500

    closing up for the night
    Signed-off-by: Schusswill <will.schuss@gmail.com>

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
+
+
+
 export class TopBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+  ) { }
 
-  ngOnInit() {
+previousPosition = (this.document.documentElement.scrollTop || this.document.body.scrollTop) + this.document.documentElement.offsetHeight;
+hide = false;
+
+@HostListener('window:scroll', [])
+onWindowScroll() {
+    const currentPosition = (document.documentElement.scrollTop || this.document.body.scrollTop) +
+     this.document.documentElement.offsetHeight;
+    if (this.previousPosition > currentPosition) {
+      this.hide = false;
+    } else {
+      this.hide = true;
+    }
+    this.previousPosition = currentPosition;
+  }
+
+  calculateState() {
+    if (this.hide) {
+      return {top: 0};
+    } else {
+      return {top: '-55px'};
+    }
+  }
+
+ngOnInit() {
   }
 
 }
